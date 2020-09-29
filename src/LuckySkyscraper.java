30eac1baf5f3ec92143cd45801a27b16f46dc4c3
postila <isabella.postila@hotmail.com>
2020-09-29 11:01:29 +0200

    Added som code to test the class Lucky Skyscraper

--- src/LuckySkyscraper.java
+++ src/LuckySkyscraper.java
@@ -1,2 +1,8 @@
 public class LuckySkyscraper {
+    static public void floorList(int totalRealFloors){
+        // loop through real floor numbers
+        for(var realFloor = 1; realFloor <= totalRealFloors; realFloor++){
+            System.out.println("realFloorNo: " + realFloor);
+        }
+    }
 }
